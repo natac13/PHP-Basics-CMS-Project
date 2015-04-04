b2baa4d5df230552cb0d9f761904e4dfaf366e95
Natac13 <Natac13@user.noreply.github.com>
2015-04-04 12:53:38 -0400

    made the nav into a function so that the manage_content.php page(main page) looks clearer! This is a very good example of refactoring code

--- includes/functions.php
+++ includes/functions.php
@@ -31,4 +31,44 @@
         confirm_query($page_set);
         return $page_set;
     }
+
+    function navigation ($subject_id, $page_id) {
+             $output = "<ul class=\"subjects\">";
+             $subject_set = find_all_subjects();
+             while($subject = mysqli_fetch_assoc($subject_set)) {
+             // this php is for only an <li> tag to either have a class of selected or not
+             // but I use the php to output everything including the normal <li> tag
+             $output .= "<li";
+             if ($subject["id"] == $subject_id) {
+                $output .=  " class=\"selected\"";
+                }
+            $output .=  ">" ;
+            $output .= "<a href=\"manage_content.php?subject=";
+            $output .=  urldecode($subject["id"]);
+            $output .= "\">";
+            $output .=  $subject["menu_name"];
+            $output .= "</a>";
+
+            $page_set = find_pages_for_subject($subject["id"]);
+            $output .= "<ul class=\"pages\">";
+             while($page = mysqli_fetch_assoc($page_set)) {
+                $output .=  "<li";
+                if ($page["id"] == $page_id) {
+                    $output .=  " class=\"selected\"";
+                }
+                $output .=  ">" ;
+                $output .= "<a href=\"manage_content.php?page=";
+                $output .=  urldecode($page["id"]);
+                $output .= "\">";
+                $output .=  $page["menu_name"] ;
+                $output .= "</a></li>";
+            }
+            mysqli_free_result($page_set);
+            $output .= "</ul> </li>";
+
+        }
+         mysqli_free_result($subject_set);
+        $output .= "</ul>";
+        return $output;
+    }
  ?>
--- public/manage_content.php
+++ public/manage_content.php
@@ -18,46 +18,7 @@
 
 <div class="main">
     <nav>
-        <ul class="subjects">
-            <?php $subject_set = find_all_subjects(); ?>
-            <?php
-                while($subject = mysqli_fetch_assoc($subject_set)) {
-            ?>
-            <!--  this php is for only an <li> tag to either have a class of selected or not
-            but I use the php to output everything including the normal <li> tag -->
-                <?php
-                    echo "<li";
-                    if ($subject["id"] == $selected_subject_id) {
-                        echo " class=\"selected\"";
-                    }
-                    echo ">" ;
-                ?>
-                    <a href="manage_content.php?subject=<?php echo urldecode($subject["id"]) ?>"><?php echo $subject["menu_name"]; ?></a>
-                    <?php $page_set = find_pages_for_subject($subject["id"]); ?>
-                    <ul class="pages">
-                        <?php
-                            while($page = mysqli_fetch_assoc($page_set)) {
-                        ?>
-                        <?php
-                            echo "<li";
-                            if ($page["id"] == $selected_page_id) {
-                                echo " class=\"selected\"";
-                            }
-                            echo ">" ;
-                        ?>
-                            <a href="manage_content.php?page=<?php echo urldecode($page["id"]) ?>"><?php echo $page["menu_name"] ; ?></a>
-                        </li>
-                        <?php
-                            }
-                         ?>
-                         <?php mysqli_free_result($page_set); ?>
-                    </ul>
-                </li>
-            <?php
-                }
-             ?>
-             <?php mysqli_free_result($subject_set); ?>
-        </ul>
+        <?php echo navigation ($selected_subject_id, $selected_page_id)  ?>
     </nav>
     <div id="page">
         <h2>Manage Content</h2>
